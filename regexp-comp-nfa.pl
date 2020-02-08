5145419607f863fd7912d609f26766109e5898dd
xN1c0 <plebani99@gmail.com>
2020-02-08 12:01:19 +0100

    Added nfa_rec

--- regexp-comp-nfa.pl
+++ regexp-comp-nfa.pl
@@ -62,60 +62,96 @@ nfa_delta(xxx, 1, a, 2).
 nfa_regexp_comp(FA_Id, RE) :-
     nonvar(FA_Id),
     is_regexp(RE),
-    nfa_regexp_comp_helper(FA_Id, RE, Start, Final),
+    RE =.. Rs,
+    nfa_regexp_comp_helper(FA_Id, Rs, Start, Final),
+    assert(nfa_start(Fa_Id, Start)),
+    assert(nfa_final(Fa_Id, Final)),
     !.
 
-nfa_regexp_comp_helper(FA_Id, RE, Start, Final) :-
-    atomic(RE),
+nfa_regexp_comp_helper(FA_Id, [Rs], Start, Final) :-
+    atomic(Rs),
     gensym(q_, Start),
     gensym(q_, Final),
-    assert(nfa_delta(Fa_Id, RE, Start, Final)),
+    assert(nfa_delta(Fa_Id, Rs, Start, Final)),
     !.
 
+/*
+STAR
+*/
+nfa_regexp_comp_helper(Fa_Id, ['*'|Rs], Start, Final) :-
+    gensym(q_, Start),
+    gensym(q_, Final),
+    nfa_regexp_comp_helper(Fa_Id, Rs, S, F),
+    assert(nfa_delta(Fa_Id, epsilon, Start, Final)),
+    assert(nfa_delta(Fa_Id, epsilon, Start, S)),
+    assert(nfa_delta(Fa_Id, epsilon, F, Final)),
+    assert(nfa_delta(Fa_Id, epsilon, F, S)),
+    !.
+
+/*
+PLUS
+*/
+nfa_regexp_comp_helper(Fa_Id, ['+'|Rs], Start, Final) :-
+    nfa_regexp_comp_helper(Fa_Id, [Rs,*(Rs)]),
+    !.
+
+/*
+SEQUENCE
+*/
 nfa_regexp_comp_helper(Fa_Id, ['[|]'|Rs], Start, Final) :-
     nfa_regexp_comp_helper_sequence(Fa_Id, Rs, Start, Final),
     !.
 
 nfa_regexp_comp_helper_sequence(Fa_Id, [], Final, Final).
 
-nfa_regexp_comp_helper_sequence(Fa_Id, [L], Start, Final) :-
-    nfa_recompile_helper(Fa_Id, L, S, F),
-    assert(nfa_delta(Fa_Id, L, epsilon, S, F)),
-    nfa_regexp_comp_helper_sequence(Fa_Id, Rs, Start, Final).
-
 nfa_regexp_comp_helper_sequence(Fa_Id, [First|Others], Start, Final) :-
-    nfa_recompile_helper(Fa_Id, First, S, F),
+    nfa_regexp_comp_helper(Fa_Id, First, S, F),
     assert(nfa_delta(Fa_Id, epsilon, Start, S)),
     nfa_regexp_comp_helper_sequence(Fa_Id, Others, F, Final),
     !.
 
-
-nfa_regexp_comp_split_case(Fa_Id, ['+'|Rs], Start, Final) :-
-    nfa_regexp_comp_case_plus(Fa_Id, Rs, Start, Final),
+/*
+OR
+*/
+nfa_regexp_comp_helper(Fa_Id, ['/'|Rs], Start, Final) :-
+    gensym(q_, Start),
+    gensym(q_,Final),
+    nfa_regexp_comp_helper_or(Fa_Id, Rs, S, F),
     !.
 
-nfa_regexp_comp_case_plus(Fa_Id, [], State1, State2) :-
-    State2 = gensym(q_, Unique).
+nfa_regexp_comp_helper_or(Fa_Id, [], Final, Final).
 
-nfa_regexp_comp_case_plus(Fa_Id, Rs, Sate1, State2) :-
-    nfa_delta(FA_Id, State1, epsilon, State2),
-    nfa_delta(Fa_Id, State2).
-
-nfa_regexp_comp_split_case(Fa_Id, ['*'|Rs], Start, Final) :-
-    !.
-
-nfa_regexp_comp_split_case(Fa_Id, ['/'|Rs], Start, Final) :-
-    !.
-
-nfa_regexp_comp_split_case(Fa_Id, ['[|]'|Rs], Start, Final) :-
+nfa_regexp_comp_helper_or(Fa_Id, [First|Others], Start, Final) :-
+    nfa_regexp_comp_helper(Fa_Id, First, S, F),
+    assert(nfa_delta(Fa_Id, epsilon, Start, S)),
+    assert(nfa_delta(Fa_Id, epsilon, F, Final)),
+    nfa_regexp_comp_helper_or(Fa_Id, Others, Start, Final),
     !.
 
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
+nfa_rec(Fa_Id, Input) :-
+    nfa_start(Fa_Id, Start),
+    nfa_rec(Fa_Id, Input, Start, Final),
+    !,
+    nfa_final(Fa_Id, Final).
 
+nfa_rec(Fa_Id, [], Final, Final) :-
+    !.
+
+nfa_rec(Fa_Id, [First|Others], Start, Final) :-
+    nfa_delta(Fa_Id, First, Start, X),
+    nfa_rec(Fa_Id, Others, X, Final),
+    !.
+
+nfa_rec(Fa_Id, Input, Start, Final) :-
+    nfa_delta(Fa_Id, epsilon, Start, X),
+    nfa_rec(Fa_Id, Input, X, Final),
+    !.
 
 
 
